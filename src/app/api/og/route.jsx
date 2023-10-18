2bc0cf53c597578dde94f747d9c103463b76c9d3
saurav903 <sk8517167@gmail.com>
2023-10-18 16:37:41 +0530

    tailwind with shadcn added and also some config with iframe design done

--- src/app/api/og/route.jsx
+++ src/app/api/og/route.jsx
@@ -4,28 +4,79 @@ import { ImageResponse } from 'next/server';
  
 export const runtime = 'edge';
  
-export async function GET() {
-  return new ImageResponse(
-    (
-      <div
-        style={{
-          fontSize: 40,
-          color: 'black',
-          background: 'white',
-          width: '100%',
-          height: '100%',
-          padding: '50px 200px',
-          textAlign: 'center',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        👋 Hello
-      </div>
-    ),
-    {
-      width: 1080,
-      height: 1920,
-    },
-  );
+export async function GET(request) {
+  try {
+    const { searchParams } = new URL(request.url);
+ 
+    // ?title=<title>
+    const hasTitle = searchParams.get('title');
+    const hasDesc = searchParams.get('description');
+    const hasWidth = searchParams.get('width');
+    const hasHeight = searchParams.get('height');
+    const title = hasTitle
+      ? searchParams.get('title')?.slice(0, 100)
+      : 'My default title';
+
+      const description = hasDesc ? searchParams.get('description')?.slice(0,100) : 'My default description';
+      const width = hasWidth ? searchParams.get('width')?.slice(0,100) : 1080;
+      const height = hasHeight ? searchParams.get('height')?.slice(0,100) : 1035;
+ 
+
+    return new ImageResponse(
+      (
+        <div
+          style={{
+            backgroundColor: 'black',
+            backgroundSize: '150px 150px',
+            height: '100%',
+            width: '100%',
+            display: 'flex',
+            textAlign: 'center',
+            alignItems: 'center',
+            justifyContent: 'center',
+            flexDirection: 'column'
+          }}
+        >
+          <div
+            style={{
+              fontSize: 60,
+              fontStyle: 'normal',
+              letterSpacing: '-0.025em',
+              color: 'white',
+              marginTop: 30,
+              padding: '0 120px',
+              lineHeight: 1.4,
+              whiteSpace: 'pre-wrap',
+            }}
+          >
+            {title}
+          </div>
+          <div
+            style={{
+              fontSize: 60,
+              fontStyle: 'normal',
+              letterSpacing: '-0.025em',
+              color: 'white',
+              marginTop: 30,
+              padding: '0 120px',
+              lineHeight: 1.4,
+              whiteSpace: 'pre-wrap',
+            }}
+          >
+            {description}
+          </div>
+          
+        </div>
+      ),
+      {
+        width: width,
+        height: height,
+      },
+    );
+  } catch (e) {
+    console.log(`${e.message}`);
+    return new Response(`Failed to generate the image`, {
+      status: 500,
+    });
+  }
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,34 +1,58 @@
+"use client"
 import Image from 'next/image'
-import styles from './page.module.css'
+// import styles from './page.module.css'
+import { useState } from 'react'
 import Link from 'next/link'
 
+
+
 export default function Home() {
+  const [state,setState]=useState({
+    title:"",
+    description:"",
+    width:1080,
+    height:1320
+  })
+
+
+  const handleChange = (e)=>{
+    let name = e.target.name;
+    let value = e.target.value;
+    setState({...state,[name]:value})
+  }
+
+  console.log(state);
+ 
   return (
     <main>
       
 
      
 
-      <div style={{margin:"auto"}}>
-        {/* <Link
-          href="/api/og"
-          target="_blank"
-    
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </Link> */}
-
-       
-
-       
-
-        <iframe width={"90%"} height={"600px"} src='/api/og'/>
+      <div style={{display:"flex"}}>
 
         
+        <div style={{flex:"5"}}>
+        <h1 className="text-2xl font-bold underline">
+      Hello world!
+    </h1>
+        <input className='border border-gray-500' type='text' name='title' onChange={handleChange}/>
+        <input className='border border-gray-500' type='text' name='description' onChange={handleChange}/>
+        <button className='border border-gray-500' onClick={()=>setState({...state,width:1080,height:1350})}>Instagram</button>
+        <button className='border border-gray-500' onClick={()=>setState({...state,width:1080,height:1920})}>Whatsapp</button>
+        </div>
+
+        <div style={{flex:"5"}}>
+        <iframe style={{minWidth:"100%",minHeight:"90vh",display:"flex",justifyContent:"center",alignItems:"center",border:"0px",borderRadius:"10px"}} src={`http://localhost:3000/api/og?title=${state.title}&description=${state.description}&width=${state.width}&height=${state.height}`} allowFullScreen></iframe>
+        </div>
+
+      
       </div>
+
+      
+
+
+      {/* <Link href={`/api/og?title=have`}>button</Link> */}
     </main>
   )
 }
