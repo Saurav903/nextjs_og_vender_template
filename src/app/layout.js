f7421446ebd56637a77a890f812e87a1f7d88584
saurav903 <sk8517167@gmail.com>
2023-11-11 00:15:00 +0530

    instagram responsive better

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"],display: 'swap', });
 
 export const metadata = {
   title: "Irctc Vendor's Form",
